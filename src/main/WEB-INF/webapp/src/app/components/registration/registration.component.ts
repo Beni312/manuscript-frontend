b1405848517595f394fb46ce6bb29057d5135cf9
Beni312 <bence312@gmail.com>
2017-11-30 00:54:15 +0100

    css dismissal to scss

--- src/main/WEB-INF/webapp/src/app/components/registration/registration.component.ts
+++ src/main/WEB-INF/webapp/src/app/components/registration/registration.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Http} from '@angular/http';
-import {Router} from '@angular/router';
 import 'rxjs/add/operator/map';
 import {RegistrationService} from '../../services/registration.service';
 import {AcademicDiscipline} from '../../models/academic.discipline';
@@ -8,7 +6,7 @@ import {AcademicDiscipline} from '../../models/academic.discipline';
 @Component({
     selector: 'app-registration',
     templateUrl: './registration.component.html',
-    styleUrls: ['./registration.component.css']
+    styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
 
@@ -20,8 +18,8 @@ export class RegistrationComponent implements OnInit {
     async ngOnInit() {
       this.service.preload().subscribe(
         academicDisciplines => this.academicDisciplines = academicDisciplines,
-          err=> {
-            console.log(err)
+          err => {
+            console.log(err);
           }
       );
     }
--- src/main/WEB-INF/webapp/src/app/shared/manage/manage.component.ts
+++ src/main/WEB-INF/webapp/src/app/shared/manage/manage.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
-  styleUrls: ['./manage.component.css']
+  styleUrls: ['./manage.component.scss']
 })
 export class ManageComponent {
 
